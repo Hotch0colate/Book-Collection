0a2ef6aa31960fdb865fbe3f668d59b5c2ef17cd
Hotch0colate <PongpayomMP@gmail.com>
2023-06-08 13:39:50 +0700

    edit a little
    some decorate

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getNumberBookInSeries, getAllSeries } from "../services/request";
 import "../styles/Landing.css";
 import Navlanding from "../components/Navlanding";
@@ -12,6 +12,8 @@ function Landing() {
   const [filterName, setFilterName] = useState("");
   let indexBigCard = -1;
   const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAllSeries().then((series) => {
@@ -50,18 +52,30 @@ function Landing() {
                 <Card
                 bookSeries="Blue Lock"
                 imgPath="https://prodimage.images-bn.com/pimages/9781646516544_p0_v3_s1200x630.jpg"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
               />
               </div>
               <div className="Follow">
                 <Card
                 bookSeries="Dandadan"
                 imgPath="https://cdn-local.mebmarket.com/meb/server1/185754/Thumbnail/book_detail_large.gif?2"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
               />
               </div> 
               <div className="Follow">
                 <Card
                 bookSeries="DR.STONE"
                 imgPath="https://cdn-local.mebmarket.com/meb/server1/202934/Thumbnail/book_detail_large.gif?2"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
               />
               </div>     
             </div>
@@ -71,19 +85,31 @@ function Landing() {
               <div className="card-landing2">
                 <Card
                 bookSeries="โตเกียว รีเวนเจอร์ส"
-                  imgPath="https://cdn-local.mebmarket.com/meb/server1/198224/Thumbnail/book_detail_large.gif?2"
+                imgPath="https://cdn-local.mebmarket.com/meb/server1/198224/Thumbnail/book_detail_large.gif?2"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
                 />
               </div>
               <div className="Follow">
                 <Card
                 bookSeries="KAIJYU No.8"
-                  imgPath="https://upload.wikimedia.org/wikipedia/en/c/cd/Kaiju_No_8.jpg"
+                imgPath="https://upload.wikimedia.org/wikipedia/en/c/cd/Kaiju_No_8.jpg"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
                 />
               </div>
               <div className="Follow">
                 <Card
                 bookSeries="Chain Saw Man"
-                  imgPath="https://animatebkk-online.com/wp-content/uploads/2021/05/ch1.jpg"
+                imgPath="https://animatebkk-online.com/wp-content/uploads/2021/05/ch1.jpg"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
                 />
               </div>
             </div>
@@ -93,19 +119,31 @@ function Landing() {
               <div className="card-landing3">
               <Card
                 bookSeries="WAKE UP WITH THE KISS"
-                  imgPath="https://cdn-local.mebmarket.com/meb/server1/178318/Thumbnail/book_detail_large.gif?2"
+                imgPath="https://cdn-local.mebmarket.com/meb/server1/178318/Thumbnail/book_detail_large.gif?2"
+                onclick={() => {
+                  navigate('/login');
+                }
+              } 
               />
               </div>
               <div className="Follow">
                 <Card
                 bookSeries="เกิดใหม่เป็นลูกโอชิ"
-                  imgPath="https://1.bp.blogspot.com/-o53FVIwZxq0/YPw8UqHSd0I/AAAAAAAAAbc/thauiZwd2TIARbb_ss9eWcPtnHBWNUw_wCLcBGAsYHQ/s1417/Mang%25C3%25A1%2B%2527Oshi%2Bno%2BKo%2527%2Bultrapassa%2B1%252C5%2Bmilh%25C3%25B5es%2Bde%2Bc%25C3%25B3pias%2Bem%2Bcircula%25C3%25A7%25C3%25A3o.jpg"
+                imgPath="https://1.bp.blogspot.com/-o53FVIwZxq0/YPw8UqHSd0I/AAAAAAAAAbc/thauiZwd2TIARbb_ss9eWcPtnHBWNUw_wCLcBGAsYHQ/s1417/Mang%25C3%25A1%2B%2527Oshi%2Bno%2BKo%2527%2Bultrapassa%2B1%252C5%2Bmilh%25C3%25B5es%2Bde%2Bc%25C3%25B3pias%2Bem%2Bcircula%25C3%25A7%25C3%25A3o.jpg"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
                 />
               </div>
               <div className="Follow">
                 <Card
                 bookSeries="Chain Saw Man"
-                  imgPath="https://animatebkk-online.com/wp-content/uploads/2021/05/ch1.jpg"
+                imgPath="https://animatebkk-online.com/wp-content/uploads/2021/05/ch1.jpg"
+                onclick={() => {
+                  navigate('/login');
+                }
+              }
                 />
               </div>
             </div>           
@@ -163,3 +201,5 @@ function Landing() {
 }
 
 export default Landing;
+
+
